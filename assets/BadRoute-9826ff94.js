0d034e52205a3090e766984e6dabd08a8806e862
NojusM <nojusmakulavicius@inbox.lt>
2023-03-14 20:36:14 +0200

    Updates

---
+++ assets/BadRoute-9826ff94.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-a69604ac.js";function o(){return e.jsx("div",{className:"center badroute",children:"404 Page not found"})}export{o as default};
---
+++ assets/Home-251944c9.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-a69604ac.js";function s(){return e.jsxs("div",{className:"center home",children:[e.jsx("h1",{children:"Hello!"}),e.jsx("p",{children:"Welcome to my demo project page"}),e.jsx("p",{children:e.jsx("b",{children:"By Nojus Makulavičius"})})]})}export{s as default};
