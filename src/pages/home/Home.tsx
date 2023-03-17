84c0ce6f8f3e884a1bbab141e1d5476a941fec45
NojusM <nojusmakulavicius@inbox.lt>
2023-03-17 18:25:34 +0200

    ADD IMAGE

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,8 +1,11 @@
 import "./home.css";
+import img from "../../assets/me.png";
+
 function Home() {
   return (
     <div className="center home">
       <h1>Hello!</h1>
+      <img src={img} alt="loading..." />
       <p>Welcome to my demo project page</p>
       <p>
         <b>By Nojus Makulavičius</b>
