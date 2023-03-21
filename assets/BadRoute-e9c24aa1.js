5904b5852cb6a0a951db98756486859a109878c7
NojusM <nojusmakulavicius@inbox.lt>
2023-03-21 18:14:20 +0200

    Updates

--- assets/BadRoute-e9c24aa1.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-d65975cb.js";function o(){return e.jsx("div",{className:"center badroute",children:"404 Page not found"})}export{o as default};
---
+++ assets/BadRoute-f2516002.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-01cb8916.js";function o(){return e.jsx("div",{className:"center badroute",children:"404 Page not found"})}export{o as default};
