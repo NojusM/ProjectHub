cc4774f6d29171052124525dbbb2a767f8361d1e
NojusM <nojusmakulavicius@inbox.lt>
2023-07-22 19:09:53 +0300

    RM WEBSITE DEMOS

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -5,8 +5,6 @@ function NavBar() {
   return (
     <nav>
       <NavLink to="/">Home</NavLink>
-      <NavLink to="/websites">Website Demos</NavLink>
-      {/* <DropdownMenu name={"Website demos"} links={URLs.websites} /> */}
       <DropdownMenu name={"Games"} links={URLs.games} />
       <DropdownMenu name={"Functional puzzles"} links={URLs.puzzles} />
     </nav>
