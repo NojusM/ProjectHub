2cb3a7cb3aefaaff40f70d4841af72e1344ded14
NojusM <nojusmakulavicius@inbox.lt>
2023-07-24 20:04:16 +0300

    UPDATE HOME TEXT

--- src/pages/pokeShop/pages/PokemonHome.tsx
+++ src/pages/pokeShop/pages/PokemonHome.tsx
@@ -2,12 +2,14 @@ export default function PokemonHome() {
   return (
     <div className="pokemon-home">
       <h1>Welcome to Pokemon center!</h1>
-      <p>This is a demo e-shop selling pokemon related things!</p>
       <p>
+        This is a demo e-shop selling pokemon related things! <br />
         Created using the public{" "}
         <a href="https://pokeapi.co" target="_blank" rel="noopener noreferrer">
           POKEMON API
         </a>
+        <br />
+        Some features might not work yet!
       </p>
     </div>
   );
