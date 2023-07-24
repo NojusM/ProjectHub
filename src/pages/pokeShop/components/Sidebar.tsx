c4dd236af114b6bdc3eb0c3ce4510ecc9b9001c2
NojusM <nojusmakulavicius@inbox.lt>
2023-07-24 15:51:33 +0300

    ADD SIDEBAR

--- src/pages/pokeShop/components/Sidebar.tsx
+++ src/pages/pokeShop/components/Sidebar.tsx
@@ -1,5 +1,23 @@
-import React from "react";
+import { useState } from "react";
 
 export default function Sidebar() {
-  return <div>Sidebar</div>;
+  const [sortAsc, setSortAsc] = useState(true);
+  const [price, setPrice] = useState([0, 100000]);
+  const [baseExp, setBaseExp] = useState([0, 100000]);
+  const [height, setHeight] = useState([0, 100000]);
+  const [weight, setWeight] = useState([0, 100000]);
+
+  return (
+    <div className="sidebar-wrapper">
+      <div className="sort">
+        Sort:
+        <button className="button">ASC</button>
+        <button className="button">DESC</button>
+      </div>
+      <div className="price"></div>
+      <div className="baseExp"></div>
+      <div className="height"></div>
+      <div className="weight"></div>
+    </div>
+  );
 }
