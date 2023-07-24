9d75902207d4c0339f2eceeab6519732065da7e6
NojusM <nojusmakulavicius@inbox.lt>
2023-07-24 15:02:47 +0300

    ADD PAGINATION

--- src/components/ItemGrid.tsx
+++ src/components/ItemGrid.tsx
@@ -6,12 +6,18 @@ interface Props {
 
 export default function ItemGrid({ data }: Props) {
   const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage, setItemsPerPage] = useState(10);
+
+  const totalPages = Math.ceil(data.length / itemsPerPage);
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const paginatedData = data.slice(startIndex, endIndex);
 
   return (
     <div className="pokeshop-wrapper">
       <div className="pokeshop-items">
         <ul className="pokemon-grid-wrapper">
-          {data.map((pokemon: any) => (
+          {paginatedData.map((pokemon: any) => (
             <div key={pokemon.name} className="pokemon-grid-item">
               <div className="pokemon-content">
                 <p>{pokemon.name}</p>
@@ -32,9 +38,24 @@ export default function ItemGrid({ data }: Props) {
           <button
             className="button"
             onClick={() => setCurrentPage((prev) => prev + 1)}
+            disabled={currentPage === totalPages}
           >
             Next Page
           </button>
+        </div>
+        <div className="pokeshop-item-limit">
+          <label>
+            Items per page:
+            <select
+              value={itemsPerPage}
+              onChange={(e) => setItemsPerPage(Number(e.target.value))}
+            >
+              <option value={10}>10</option>
+              <option value={20}>20</option>
+              <option value={50}>50</option>
+              <option value={100}>100</option>
+            </select>
+          </label>
         </div>
       </div>
     </div>
