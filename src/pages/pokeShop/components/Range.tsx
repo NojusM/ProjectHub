1e77b72db4d02b7bc057f9f154491a042422d54b
NojusM <nojusmakulavicius@inbox.lt>
2023-07-24 16:20:27 +0300

    GENERALIZE VAR NAMES

--- src/pages/pokeShop/components/Range.tsx
+++ src/pages/pokeShop/components/Range.tsx
@@ -5,14 +5,14 @@ interface Props {
 }
 
 export default function Range({ label, data, setData }: Props) {
-  const handlePriceChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleRangeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = parseInt(e.target.value);
-    if (e.target.name === "minPrice") {
+    if (e.target.name === "min") {
       setData((current) => [value, current[1]]);
       if (value > data[1]) {
         setData((current) => [current[0], value]);
       }
-    } else if (e.target.name === "maxPrice") {
+    } else if (e.target.name === "max") {
       setData((current) => [current[0], value]);
       if (value < data[0]) {
         setData((current) => [value, current[1]]);
@@ -22,23 +22,23 @@ export default function Range({ label, data, setData }: Props) {
 
   return (
     <div className="range-wrapper">
-      <label htmlFor="priceRange">{label}</label>
+      <label htmlFor="range">{label}</label>
       <input
         type="range"
-        id="priceRange"
-        name="minPrice"
+        id="range"
+        name="min"
         min={0}
         max={100}
         value={data[0]}
-        onChange={handlePriceChange}
+        onChange={handleRangeChange}
       />
       <input
         type="range"
-        name="maxPrice"
+        name="max"
         min={0}
         max={100}
         value={data[1]}
-        onChange={handlePriceChange}
+        onChange={handleRangeChange}
       />
       <span>{data[0]}</span> - <span>{data[1]}</span>
     </div>
