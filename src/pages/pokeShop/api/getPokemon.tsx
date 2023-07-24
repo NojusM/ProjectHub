e52b8f7b5ef7cf984c3919df62f7247b89fe021f
NojusM <nojusmakulavicius@inbox.lt>
2023-07-24 18:20:26 +0300

    ADD PRICE TO POKEMON

--- src/pages/pokeShop/api/getPokemon.tsx
+++ src/pages/pokeShop/api/getPokemon.tsx
@@ -10,7 +10,10 @@ export async function getPokemon() {
     const extraInfoResponses = await Promise.all(
       data.results.map((pokemon: any) => axios.get(pokemon.url))
     );
-    const pokemon = extraInfoResponses.map((res) => res.data);
+    const pokemon = extraInfoResponses.map((res) => {
+      const price = Math.floor(Math.random() * 1298) + 2;
+      return { ...res.data, price: price };
+    });
 
     return { pokemon, ...data };
   } catch (error) {
