2fe86a4737a0be422f7b399b45b49f35dcdf7935
NojusM <nojusmakulavicius@inbox.lt>
2023-07-24 19:59:00 +0300

    UPDATE NAV CSS

--- src/pages/pokeShop/components/PokeshopNavbar.tsx
+++ src/pages/pokeShop/components/PokeshopNavbar.tsx
@@ -2,11 +2,19 @@ import { NavLink } from "react-router-dom";
 
 export default function PokeshopNavbar() {
   return (
-    <nav>
-      <NavLink to="/pokeshop">Home</NavLink>
-      <NavLink to="/pokeshop/pokemon">Pokemon</NavLink>
-      <NavLink to="/pokeshop/berries">Berries</NavLink>
-      <NavLink to="/pokeshop/cart">Cart</NavLink>
+    <nav className="pokemon-navbar">
+      <NavLink to="/pokeshop" end>
+        <div className="nav-container">Home</div>
+      </NavLink>
+      <NavLink to="/pokeshop/pokemon">
+        <div className="nav-container">Pokemon</div>
+      </NavLink>
+      <NavLink to="/pokeshop/berries">
+        <div className="nav-container">Berries</div>
+      </NavLink>
+      <NavLink to="/pokeshop/cart">
+        <div className="nav-container">Cart</div>
+      </NavLink>
     </nav>
   );
 }
