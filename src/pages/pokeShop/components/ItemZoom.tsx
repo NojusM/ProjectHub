800de5c0bc3fc4c07c25898699fb0909e063a295
NojusM <nojusmakulavicius@inbox.lt>
2023-07-26 15:38:34 +0300

    DISPLAY BASIC INFO

--- src/pages/pokeShop/components/ItemZoom.tsx
+++ src/pages/pokeShop/components/ItemZoom.tsx
@@ -1,6 +1,24 @@
+import { useState } from "react";
+import { useQueryClient } from "react-query";
 import { useParams } from "react-router-dom";
+import { Pokemon, PokemonApiResponse } from "../../../types/pokeshopTypes";
 
 export default function ItemZoom() {
   const { name } = useParams();
-  return <div>{name}</div>;
+  const queryClient = useQueryClient();
+  const pokemonData = queryClient.getQueryData("pokemon") as PokemonApiResponse;
+  if (!pokemonData?.pokemon) {
+    return <div>Pokemon have ran away :{"("}</div>;
+  }
+
+  const pokemon = pokemonData?.pokemon.find((poke) => poke.name === name);
+
+  return !pokemon ? (
+    <div>Pokemon not found!</div>
+  ) : (
+    <div>
+      {pokemon.height}
+      {pokemon.name}
+    </div>
+  );
 }
