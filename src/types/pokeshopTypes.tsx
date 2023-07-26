0eb60f88e94feeaa5cd8f63b38895b424bd3dee9
NojusM <nojusmakulavicius@inbox.lt>
2023-07-26 15:24:58 +0300

    ADD POKEMON TYPE

--- src/types/pokeshopTypes.tsx
+++ src/types/pokeshopTypes.tsx
@@ -18,3 +18,36 @@ export interface RangesData {
   title: string;
   units: string;
 }
+
+type NameUrl = { name: string; url: string };
+export interface Pokemon {
+  id: number;
+  name: string;
+  base_experience: number;
+  height: number;
+  is_default: boolean;
+  order: number;
+  weight: number;
+  abilities: { is_hidden: boolean; slot: number; ability: NameUrl };
+  forms: NameUrl[];
+  game_indices: { game_index: number; version: NameUrl }[];
+  held_items: { item: NameUrl; version: { version: NameUrl; rarity: number } }[];
+  location_area_encounters: string;
+  moves: {
+    move: NameUrl;
+    version_group_details: { move_learn_method: NameUrl; version_group: NameUrl; level_learned_at: number }[];
+  }[];
+  past_types: { generation: NameUrl; types: { slot: number; type: NameUrl }[] }[];
+  sprites: {
+    front_default: string;
+    front_shiny: string;
+    front_female: string;
+    front_shiny_female: string;
+    back_default: string;
+    back_female: string;
+    back_shiny_female: string;
+  };
+  species: NameUrl;
+  stats: { stat: NameUrl; effort: number; base_stat: number }[];
+  types: { slot: number; type: NameUrl }[];
+}
