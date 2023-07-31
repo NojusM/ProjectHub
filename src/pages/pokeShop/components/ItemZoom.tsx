5e55c5ac01f3647957d6aeb2ade8c282dc694d05
Makulavičius Nojus <nojus.makulavicius@mif.stud.vu.lt>
2023-07-31 16:21:49 +0300

    ADD IMG SELECTION

--- src/pages/pokeShop/components/ItemZoom.tsx
+++ src/pages/pokeShop/components/ItemZoom.tsx
@@ -7,20 +7,40 @@ export default function ItemZoom() {
   const { name } = useParams();
   const queryClient = useQueryClient();
   const pokemonData = queryClient.getQueryData("pokemon") as PokemonApiResponse;
-  if (!pokemonData?.pokemon) {
-    return <div>Pokemon have ran away :{"("}</div>;
+  const pokemon = pokemonData?.pokemon?.find((poke) => poke.name === name);
+  if (!pokemon) {
+    return <div>Not found</div>;
   }
+  const [selected, setSelected] = useState(1);
 
-  const pokemon: Pokemon | undefined = pokemonData?.pokemon.find(
-    (poke) => poke.name === name
-  );
-
-  return !pokemon ? (
-    <div>Pokemon not found!</div>
-  ) : (
-    <div>
-      {pokemon.height}
-      {pokemon.name}
+  return (
+    <div className="zoom-wrapper">
+      <div className="zoom-header">
+        <div className="zoom-image">
+          <div className="zoom-image-selector">
+            <img
+              src={pokemon.sprites.front_default}
+              className={selected === 1 ? "active" : ""}
+              onClick={() => setSelected(1)}
+            />
+            <img
+              src={pokemon.sprites.back_default}
+              className={selected === 2 ? "active" : ""}
+              onClick={() => setSelected(2)}
+            />
+          </div>
+          <div className="zoom-image-selected">
+            {selected === 1 ? (
+              <img src={pokemon.sprites.front_default} />
+            ) : (
+              <img src={pokemon.sprites.back_default} />
+            )}
+          </div>
+        </div>
+        <div className="zoom-buy">
+          {pokemon.price} <button>Buy</button>
+        </div>
+      </div>
     </div>
   );
 }
